ad0511cdd062242ea12afd0ad4ec28f16a18e0a0
Tam Le <tamsgit@gmail.com>
2021-05-09 23:04:24 -0400

    Create model for "Post" in app "blogs"
    - Create migration schema via command:
      `./manage-prod makemigrations blogs`
    - Run migration files via command:
      `./manage-prod migrate blogs`

--- server/apps/blogs/models.py
+++ server/apps/blogs/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    head = models.CharField(max_length=255)
+    body = models.TextField(blank=True)
