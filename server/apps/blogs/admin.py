6d174f1d41237fbccf6c2953338a3fbec82b15b2
Tam Le <tamsgit@gmail.com>
2021-05-10 23:39:19 -0400

    Admin: Display other fields in list view

--- server/apps/blogs/admin.py
+++ server/apps/blogs/admin.py
@@ -5,4 +5,5 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 
+    list_display = ('id', 'author', 'head', 'created', 'modified',)
     readonly_fields = ('created', 'modified',)
