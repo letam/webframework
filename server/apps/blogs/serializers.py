d7e439bb3da96d5d838086364a39b32a8a76680b
Tam Le <tamsgit@gmail.com>
2021-05-15 22:39:57 -0400

    Add API viewset for posts; Add API root to Nginx

---
+++ server/apps/blogs/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ['url', 'author', 'head', 'body',]
--- server/config/urls.py
+++ server/config/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import routers
+
+from apps.blogs.views import PostViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api-auth/', include('rest_framework.urls')),
+
+    path('api/', include(router.urls)),
 ]
