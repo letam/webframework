84c87c745a7dd3154cede408f5dc70e3f4d90b22
Tam Le <tamsgit@gmail.com>
2021-05-24 19:21:02 -0400

    App: Login request functionality

--- app/src/components/Login.tsx
+++ app/src/components/Login.tsx
@@ -1,6 +1,26 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState, useCallback } from "react";
+
+interface ICSRFToken {
+  token: string;
+}
 
 export default function Login(): ReactElement {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = useCallback(
+    async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const data = (await (await fetch("/auth/csrf/")).json()) as ICSRFToken;
+      const csrftoken = data.token;
+      await fetch("/auth/login/", {
+        method: (event.target as HTMLFormElement).method,
+        headers: { "X-CSRFToken": csrftoken },
+        body: JSON.stringify({ username, password }),
+      });
+    },
+    [username, password]
+  );
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -11,7 +31,7 @@ export default function Login(): ReactElement {
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={handleSubmit} method="POST">
             <div>
               <label
                 htmlFor="username"
@@ -27,6 +47,8 @@ export default function Login(): ReactElement {
                   autoComplete="username"
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  onChange={(event) => setUsername(event.target.value)}
+                  value={username}
                 />
               </div>
             </div>
@@ -46,6 +68,8 @@ export default function Login(): ReactElement {
                   autoComplete="current-password"
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  onChange={(event) => setPassword(event.target.value)}
+                  value={password}
                 />
               </div>
             </div>
