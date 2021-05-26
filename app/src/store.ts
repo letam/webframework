0d2c5e3dfebf406d68d0d633ce9c221faa8bb2de
Tam Le <tamsgit@gmail.com>
2021-05-25 22:20:57 -0400

    App-store: Store and retrieve from localStorage

--- app/src/store.ts
+++ app/src/store.ts
@@ -1,15 +1,30 @@
+/* Simple Application Store */
+
 export class Store {
   store: Record<string, unknown> = {};
 
   set(key: string, value: unknown): void {
     this.store[key] = value;
+    this.storeInLocalStorage();
   }
 
   get(key: string): unknown {
     return this.store[key];
   }
+
+  storeInLocalStorage(): void {
+    localStorage.setItem("store", JSON.stringify(this.store));
+  }
+
+  restoreFromLocalStorage(): void {
+    const storeFromLocalStorage = localStorage.getItem("store");
+    if (storeFromLocalStorage) {
+      this.store = JSON.parse(storeFromLocalStorage) as Record<string, unknown>;
+    }
+  }
 }
 
 export const store = new Store();
+store.restoreFromLocalStorage();
 
 (window as any)._store = store; // eslint-disable-line
