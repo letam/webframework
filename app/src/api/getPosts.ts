25b883b009339e3b76695823ad909913795c12e1
Tam Le <tamsgit@gmail.com>
2021-05-30 22:20:05 -0400

    App-Refactor: Use constants file for env variables

--- app/src/api/getPosts.ts
+++ app/src/api/getPosts.ts
@@ -1,6 +1,5 @@
 import { IPost } from "types";
-
-const BACKEND_HOST = (import.meta.env.VITE_BACKEND_HOST as string) || "";
+import { BACKEND_HOST } from "api/constants";
 
 export default async function getPosts(): Promise<IPost[]> {
   return (await fetch(`${BACKEND_HOST}/api/posts/`)).json() as Promise<IPost[]>;
