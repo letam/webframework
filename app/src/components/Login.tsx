fc98e099d920360da38d85444360b71c3023b3bf
Tam Le <tamsgit@gmail.com>
2021-06-01 00:47:48 -0400

    App-CSS: No vertical padding for view on mobile

--- app/src/components/Login.tsx
+++ app/src/components/Login.tsx
@@ -53,7 +53,7 @@ export default function Login(): ReactElement {
     <div className="min-h-screen flex flex-col">
       <Head title="Login | wut.sh" />
       <Header />
-      <div className="flex-grow bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 dark:bg-gray-800">
+      <div className="flex-grow bg-gray-50 flex flex-col justify-center sm:py-12 sm:px-6 lg:px-8 dark:bg-gray-800">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-current">
             Sign in to your account
