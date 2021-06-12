2f3672cd018d09f84879ceb100e31092be6ef1e1
Tam Le <tamsgit@gmail.com>
2021-06-12 02:59:31 -0400

    App: Render based on auth status

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,13 +1,24 @@
-import React, { ReactElement, useEffect } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { store } from "store";
 
 export default function Header(): ReactElement {
+  const [authStatus, setAuthStatus] = useState({
+    isInitialized: false,
+    isAuthenticated: false,
+    username: "",
+  });
   useEffect(() => {
     const username = store.get("username") as string;
     if (username) {
       console.log("auth as:", username);
+      setAuthStatus((state) => ({
+        ...state,
+        isInitialized: true,
+        isAuthenticated: true,
+        username,
+      }));
     }
   }, []);
 
@@ -17,9 +28,15 @@ export default function Header(): ReactElement {
         <Link className="m-4" to="/">
           <h1>Wut?</h1>
         </Link>
-        <Link className="m-4" to="/login">
-          Login
-        </Link>
+        {!authStatus.isInitialized ? undefined : authStatus.isAuthenticated ? (
+          <button type="button" className="m-4">
+            Logout
+          </button>
+        ) : (
+          <Link className="m-4" to="/login">
+            Login
+          </Link>
+        )}
       </div>
       <hr />
     </>
