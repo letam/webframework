bd7304c53a88fc05d4cf51cb11d04892bed90c1d
Tam Le <tamsgit@gmail.com>
2021-06-14 09:55:33 -0400

    App-Refactor: Store routes in array of maps

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -7,15 +7,36 @@ const Login = lazy(() => import("./Login"));
 const FruitDetails = lazy(() => import("./FruitDetails"));
 const FruitGallery = lazy(() => import("./FruitGallery"));
 
+const routes = [
+  {
+    path: "/",
+    component: Index,
+    exact: true,
+  },
+  {
+    path: "/login",
+    component: Login,
+    exact: true,
+  },
+  {
+    path: "/fruits",
+    component: FruitGallery,
+    exact: true,
+  },
+  {
+    path: "/:fruitName",
+    component: FruitDetails,
+  },
+];
+
 export default function App(): ReactElement {
   return (
     <BrowserRouter>
       <Suspense fallback={<LoadingOrError />}>
         <Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/fruits" component={FruitGallery} />
-          <Route path="/:fruitName" component={FruitDetails} />
+          {routes.map(({ path, component, exact }) => (
+            <Route key="path" {...{ path, component, exact }} />
+          ))}
         </Switch>
       </Suspense>
     </BrowserRouter>
