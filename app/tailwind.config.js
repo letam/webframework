e1bcfaacb358fcc3397b60bc9859a566b6e42b2b
Tam Le <tamsgit@gmail.com>
2021-12-20 23:11:10 -0500

    FE: Update/Migrate Tailwind config for v3
    - Following instructions from https://tailwindcss.com/docs/upgrade-guide

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,13 +2,11 @@ const defaultConfig = require("tailwindcss/defaultConfig");
 const formsPlugin = require("@tailwindcss/forms");
 
 module.exports = {
-  mode: "jit",
-  purge: ["index.html", "src/**/*.tsx"],
+  content: ["index.html", "src/**/*.tsx"],
   theme: {
     fontFamily: {
       sans: ["Inter var", defaultConfig.theme.fontFamily.sans],
     },
   },
-  darkMode: "media",
   plugins: [formsPlugin],
 };
