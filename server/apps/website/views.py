f250f5916239c112217648229b62d310941133c3
Tam Le <tamsgit@gmail.com>
2022-01-01 21:00:25 -0500

    BE-dev: Respond with data from file in app/public
    - Return response with data from file in app/public during local
      development when serving website through Django server (i.e.
      http://localhost:8000)

--- server/apps/website/views.py
+++ server/apps/website/views.py
@@ -1,5 +1,13 @@
+from django.conf import settings
 from django.shortcuts import render
 
 def index(request):
+    if settings.DEBUG:
+        if request.path == '/fruits.json':
+            from django.http import JsonResponse
+            import json
+            with open('app/public/fruits.json') as f:
+                return JsonResponse(json.load(f), safe=False)
+
     template = 'website/index.html'
     return render(request, template)
