9fc01d6d7f75871d6511c3064fc534dfd42332e2
Tam Le <tamsgit@gmail.com>
2024-11-07 19:34:54 -0500

    DB: Use sqlite for database

--- server/config/settings_production.py
+++ server/config/settings_production.py
@@ -23,33 +23,36 @@ CORS_ALLOWED_ORIGINS.extend(
 )
 
 
-import dj_database_url
-import urllib.parse
-import os
-import getpass
+# We using SQLite, not PostgreSQL :-)
 
-with open(
-    f"/home/{os.environ.get('SUDO_USER') or getpass.getuser()}/.credentials/psql/test_zgry_dev"
-) as fd:
-    credentials = {
-        var: val.rstrip("\n")
-        for var, val in [line.split("=", 1) for line in fd if "=" in line]
-    }
 
-# Encode every special character in value, for use in URLs
-for k, v in credentials.items():
-    if k in ["db_password"]:
-        v = v.strip("'")
-    credentials[k] = urllib.parse.quote(v)
+# import dj_database_url
+# import urllib.parse
+# import os
+# import getpass
 
-db_user = credentials["db_user"]
-db_password = credentials["db_password"]
-db_host = credentials["db_host"]
-db_name = credentials["db_name"]
+# with open(
+#     f"/home/{os.environ.get('SUDO_USER') or getpass.getuser()}/.credentials/psql/test.zgry.dev"
+# ) as fd:
+#     credentials = {
+#         var: val.rstrip("\n")
+#         for var, val in [line.split("=", 1) for line in fd if "=" in line]
+#     }
 
-if db_host == "localhost":
-    db_host = ""
+# # Encode every special character in value, for use in URLs
+# for k, v in credentials.items():
+#     if k in ["db_password"]:
+#         v = v.strip("'")
+#     credentials[k] = urllib.parse.quote(v)
 
-DATABASES["default"] = dj_database_url.parse(
-    f"postgres://{db_user}:{db_password}@{db_host}/{db_name}"
-)
+# db_user = credentials["db_user"]
+# db_password = credentials["db_password"]
+# db_host = credentials["db_host"]
+# db_name = credentials["db_name"]
+
+# if db_host == "localhost":
+#     db_host = ""
+
+# DATABASES["default"] = dj_database_url.parse(
+#     f"postgres://{db_user}:{db_password}@{db_host}/{db_name}"
+# )
