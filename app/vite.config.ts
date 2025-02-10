f58b52776afbfb1a3ec445ebe50151d0f45a4ae0
Tam Le <tamsgit@gmail.com>
2025-02-09 22:14:32 -0500

    FE-app: Create new Vite-React-RS PWA app
    - Scaffolded with command:
    ```
    npm create @vite-pwa/pwa@latest app -- --template react-ts
    ```
    Prompt answers:
    ✔ PWA Name: … app
    ✔ PWA Short Name: … app
    ✔ PWA Description: …
    ✔ Theme color: … #ffffff
    ✔ Select a strategy: › injectManifest
    ✔ Select a behavior: › Prompt for update
    ✔ Enable periodic SW updates? … x no / yes
    ✔ Show offline ready prompt? … x no / yes
    ✔ Generate PWA Assets Icons on the fly? … no / x yes
    
    - Reference: https://vite-pwa-org.netlify.app/guide/#scaffolding-your-first-vite-pwa-project

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'app',
+      short_name: 'app',
+      description: 'app',
+      theme_color: '#ffffff',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
