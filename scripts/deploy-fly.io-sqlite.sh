5f3b23053eee25e1b62e58a7b1a37981ae22fd32
Tam Le <tamsgit@gmail.com>
2025-02-25 19:25:45 -0500

    BE: Update script to launch app without prompting

--- scripts/deploy-fly.io-sqlite.sh
+++ scripts/deploy-fly.io-sqlite.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Create app without launching
-fly launch --vm-memory 512 --ha=false --no-db --now --build-only
+yes n | fly launch --vm-memory 512 --ha=false --no-db --now --copy-config --build-only
 
 # Set database location
 flyctl secrets set DATABASE_URL=sqlite:////data/db.sqlite3
