cb5ef6e000254bb93e87aa2523177b9f62dcab81
Tam Le <tamsgit@gmail.com>
2025-02-27 16:02:02 -0500

    Update script to deploy app with postgres

--- scripts/deploy-fly.io-postgres.sh
+++ scripts/deploy-fly.io-postgres.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
+# Specify the target fly.toml configuration file
+function specify_target_fly_toml() {
+    # Move currently active fly.toml configuration file to local tmp directory
+    if [ -f fly.toml ]; then
+        mkdir -p .tmp
+        mv fly.toml .tmp/fly.toml
+    fi
+
+    # Copy target fly.toml configuration file to project root directory
+    cp -p ./scripts/configs/fly-postgres.toml fly.toml
+}
+specify_target_fly_toml
+
 # Launch app
-yes n | fly launch --vm-memory 512 --now --copy-config
+yes n | fly launch --now --copy-config
+
+# Restore existing fly.toml
+mv .tmp/fly.toml fly.toml
