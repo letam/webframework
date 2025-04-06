26a7695ce3bc0f7f04137c4a304f8ccaba84c26e
Tam Le <tamsgit@gmail.com>
2025-04-05 20:02:27 -0400

    add util script to convert audio file to mp3

---
+++ server/apps/blogs/utils/convert_to_mp3.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import logging
+
+logger = logging.getLogger('server.apps.blogs')
+
+
+def get_output_filename(input_file: str) -> str:
+    base, _ = os.path.splitext(input_file)
+    return base + ".mp3"
+
+def convert_to_mp3(input_file: str) -> str:
+    """
+    Converts an audio file to MP3 format using the local ffmpeg binary located in the project.
+
+    Parameters:
+        input_file (str): Path to the input audio file.
+    """
+    output_file = get_output_filename(input_file)
+
+    # TODO: Handle case when file already exists
+
+    ffmpeg_binary = "./bin/ffmpeg"
+    command = [
+        ffmpeg_binary,
+        "-i", input_file,
+        "-vn",
+        "-ar", "44100",
+        "-ac", "2",
+        "-b:a", "96k",
+        output_file
+    ]
+
+    try:
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        logger.debug("Conversion successful! Output file: %s", output_file)
+    except subprocess.CalledProcessError as error:
+        logger.error("Error during conversion:")
+        logger.error(error.stderr.decode())
+
+    return output_file
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        logger.error("No input file provided.")
+        exit(1)
+
+    input_audio = filename
+    convert_to_mp3(input_audio)
