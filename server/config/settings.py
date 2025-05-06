5b5b1d4c8a01bdccda4af9eb54112512b8f2bc5f
Tam Le <tamsgit@gmail.com>
2025-05-05 21:01:26 -0400

    revert previous commit since it doesn't work

--- server/config/settings.py
+++ server/config/settings.py
@@ -154,7 +154,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'csp.middleware.CSPMiddleware',
-    'apps.website.middleware.http.HttpRangesMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
